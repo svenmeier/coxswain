61239881d4fafcbef12a5433622faa2b01fcaecb
Sven Meier <svenmeier@apache.org>
2015-10-28 09:02:37 +0100

    substitute text in summary of preference

---
+++ app/src/main/java/svenmeier/coxswain/view/preference/EditTextPreference.java
@@ -0,0 +1,33 @@
+package svenmeier.coxswain.view.preference;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * An specialization that substitutes the current text into the summary (as ListPreference does it
+ * too).
+ */
+public class EditTextPreference extends android.preference.EditTextPreference {
+
+	public EditTextPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+	public EditTextPreference(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public EditTextPreference(Context context) {
+		super(context);
+	}
+
+	@Override
+	public CharSequence getSummary() {
+		CharSequence summary = super.getSummary();
+		if (summary == null) {
+			return summary;
+		}
+
+		return String.format(summary.toString(), getText());
+	}
+}

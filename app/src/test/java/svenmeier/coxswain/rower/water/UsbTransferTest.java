bfa9feea467fef5a3030bb72b672923f5bebe147
Sven Meier <sven@meiers.net>
2017-02-02 13:01:53 +0100

    more tests

--- app/src/test/java/svenmeier/coxswain/rower/water/UsbTransferTest.java
+++ app/src/test/java/svenmeier/coxswain/rower/water/UsbTransferTest.java
@@ -13,13 +13,14 @@ public class UsbTransferTest {
 
 	@Test
 	public void baudrate() throws Exception {
-		assertEquals(0x1A, UsbTransfer.divisor(115200));
-		assertEquals(0x9c4, UsbTransfer.divisor(1200));
+		assertEquals(0x09c4, UsbTransfer.divisor(1200));
+		assertEquals(0x001A, UsbTransfer.divisor(115200));
 	}
 
 	@Test
 	public void data() throws Exception {
 		assertEquals(0b0_0_000_000_00001000, UsbTransfer.data(8, ITransfer.PARITY_NONE, ITransfer.STOP_BIT_1_0, false));
-		assertEquals(0b0_1_010_001_00000100, UsbTransfer.data(4, ITransfer.PARITY_ODD, ITransfer.STOP_BIT_2_0, true));
+		assertEquals(0b0_0_001_001_00001000, UsbTransfer.data(8, ITransfer.PARITY_ODD, ITransfer.STOP_BIT_1_5, false));
+		assertEquals(0b0_1_010_010_00000100, UsbTransfer.data(4, ITransfer.PARITY_EVEN, ITransfer.STOP_BIT_2_0, true));
 	}
 }

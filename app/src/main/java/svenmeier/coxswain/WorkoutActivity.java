a7b400b0eb464b84a63db819ebd4a4685fc02722
Sven Meier <sven@meiers.net>
2017-05-26 17:11:53 +0200

    loaders are reset automatically, no need to handle this

--- app/src/main/java/svenmeier/coxswain/WorkoutActivity.java
+++ app/src/main/java/svenmeier/coxswain/WorkoutActivity.java
@@ -18,7 +18,6 @@ package svenmeier.coxswain;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -47,346 +46,332 @@ import svenmeier.coxswain.view.ValueBinding;
  */
 public class WorkoutActivity extends AbstractActivity implements View.OnSystemUiVisibilityChangeListener, Gym.Listener, BindingDialogFragment.Callback {
 
-    private static final List<ValueBinding> DEFAULT_BINDING = Arrays.asList(
-            ValueBinding.DURATION,
-            ValueBinding.DISTANCE,
-            ValueBinding.STROKES,
-            ValueBinding.SPEED,
-            ValueBinding.PULSE,
-            ValueBinding.STROKE_RATE);
+	private static final List<ValueBinding> DEFAULT_BINDING = Arrays.asList(
+			ValueBinding.DURATION,
+			ValueBinding.DISTANCE,
+			ValueBinding.STROKES,
+			ValueBinding.SPEED,
+			ValueBinding.PULSE,
+			ValueBinding.STROKE_RATE);
 
-    private static final List<ValueBinding> DEFAULT_PACE_BINDING = Arrays.asList(
-            ValueBinding.DURATION,
-            ValueBinding.DISTANCE,
-            ValueBinding.STROKES,
-            ValueBinding.SPEED,
-            ValueBinding.DELTA_DISTANCE,
-            ValueBinding.STROKE_RATE);
+	private static final List<ValueBinding> DEFAULT_PACE_BINDING = Arrays.asList(
+			ValueBinding.DURATION,
+			ValueBinding.DISTANCE,
+			ValueBinding.STROKES,
+			ValueBinding.SPEED,
+			ValueBinding.STROKES,
+			ValueBinding.SPEED,
+			ValueBinding.DELTA_DISTANCE,
+			ValueBinding.STROKE_RATE);
 
-    private static final int LEAN_BACK =
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-            View.SYSTEM_UI_FLAG_FULLSCREEN |
-            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+	private static final int LEAN_BACK =
+			View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+					View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+					View.SYSTEM_UI_FLAG_FULLSCREEN |
+					View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+					View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
 
-    private Gym gym;
+	private Gym gym;
 
-    private DismissablePaceBoat paceBoat;
+	private BindingView.PaceBoat paceBoat;
 
-    private Preference<ValueBinding> bindingPreference;
+	private Preference<ValueBinding> bindingPreference;
 
-    private GridLayout gridhView;
+	private GridLayout gridhView;
 
-    private SegmentsView segmentsView;
+	private SegmentsView segmentsView;
 
-    private LevelView progressView;
+	private LevelView progressView;
 
-    private Runnable returnToLeanBack = new Runnable() {
-        @Override
-        public void run() {
-            getWindow().getDecorView().setSystemUiVisibility(LEAN_BACK);
-        }
-    };
+	private Runnable returnToLeanBack = new Runnable() {
+		@Override
+		public void run() {
+			getWindow().getDecorView().setSystemUiVisibility(LEAN_BACK);
+		}
+	};
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
 
-        gym = Gym.instance(this);
+		gym = Gym.instance(this);
 
-        super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
-                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
-                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
-                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        getWindow().getDecorView().setSystemUiVisibility(LEAN_BACK);
-        getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(this);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+				WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+				WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		getWindow().getDecorView().setSystemUiVisibility(LEAN_BACK);
+		getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(this);
 
-        setContentView(R.layout.layout_workout);
-        segmentsView = (SegmentsView) findViewById(R.id.workout_segments);
-        segmentsView.setData(new SegmentsData(gym.program));
-        progressView = (LevelView) findViewById(R.id.workout_progress);
-        gridhView = (GridLayout)findViewById(R.id.workout_grid);
+		setContentView(R.layout.layout_workout);
+		segmentsView = (SegmentsView) findViewById(R.id.workout_segments);
+		segmentsView.setData(new SegmentsData(gym.program));
+		progressView = (LevelView) findViewById(R.id.workout_progress);
+		gridhView = (GridLayout) findViewById(R.id.workout_grid);
 
-        List<ValueBinding> defaultBinding;
-        if (gym.pace == null) {
-            defaultBinding = DEFAULT_BINDING;
-            bindingPreference = Preference.getEnum(this, ValueBinding.class, R.string.preference_workout_binding);
+		List<ValueBinding> defaultBinding;
+		if (gym.pace == null) {
+			defaultBinding = DEFAULT_BINDING;
+			bindingPreference = Preference.getEnum(this, ValueBinding.class, R.string.preference_workout_binding);
 
-            paceBoat = new SelfPaceBoat();
-        } else {
-            defaultBinding = DEFAULT_PACE_BINDING;
-            bindingPreference = Preference.getEnum(this, ValueBinding.class, R.string.preference_workout_binding_pace);
+			paceBoat = new SelfPaceBoat();
+		} else {
+			defaultBinding = DEFAULT_PACE_BINDING;
+			bindingPreference = Preference.getEnum(this, ValueBinding.class, R.string.preference_workout_binding_pace);
 
-            paceBoat = new WorkoutPaceBoat();
-        }
+			paceBoat = new WorkoutPaceBoat();
+		}
 
-        try {
-            writeToGrid(bindingPreference.getList());
-        } catch (Exception ex) {
-            writeToGrid(defaultBinding);
-        }
-    }
+		try {
+			writeToGrid(bindingPreference.getList());
+		} catch (Exception ex) {
+			writeToGrid(defaultBinding);
+		}
+	}
 
-    private void writeToGrid(List<ValueBinding> binding) {
-        if (binding == null || binding.isEmpty()) {
-            throw new IllegalArgumentException("binding must not be empty");
-        }
+	private void writeToGrid(List<ValueBinding> binding) {
+		if (binding == null || binding.isEmpty()) {
+			throw new IllegalArgumentException("binding must not be empty");
+		}
 
-        int columns;
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            columns = binding.size() <= 3 ? 1 : 2;
-        } else {
-            columns = binding.size() <= 6 ? 1 : 2;
-        }
-        gridhView.removeAllViews();
-        gridhView.setColumnCount(columns);
+		int columns;
+		if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+			columns = binding.size() <= 3 ? 1 : 2;
+		} else {
+			columns = binding.size() <= 6 ? 1 : 2;
+		}
+		gridhView.removeAllViews();
+		gridhView.setColumnCount(columns);
 
-        for (int b = 0; b < binding.size(); b++) {
-            ValueBinding temp = binding.get(b);
+		for (int b = 0; b < binding.size(); b++) {
+			ValueBinding temp = binding.get(b);
 
-            final BindingView bindingView = (BindingView) getLayoutInflater().inflate(R.layout.layout_workout_binding, gridhView, false);
-            gridhView.addView(bindingView);
+			final BindingView bindingView = (BindingView) getLayoutInflater().inflate(R.layout.layout_workout_binding, gridhView, false);
+			gridhView.addView(bindingView);
 
-            bindingView.setBinding(temp);
+			bindingView.setBinding(temp);
 
-            final int index = b;
-            bindingView.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View view) {
-                    leanBack(false);
+			final int index = b;
+			bindingView.setOnLongClickListener(new View.OnLongClickListener() {
+				@Override
+				public boolean onLongClick(View view) {
+					leanBack(false);
 
-                    BindingDialogFragment fragment = BindingDialogFragment.create(index, bindingView.getBinding());
+					BindingDialogFragment fragment = BindingDialogFragment.create(index, bindingView.getBinding());
 
-                    fragment.show(getFragmentManager(), "bindingPreference");
+					fragment.show(getFragmentManager(), "bindingPreference");
 
-                    return true;
-                }
-            });
-        }
+					return true;
+				}
+			});
+		}
 
-        gridhView.requestLayout();
-    }
+		gridhView.requestLayout();
+	}
 
-    @NonNull
-    private List<ValueBinding> readFromGrid() {
-        List<ValueBinding> bindings = new ArrayList<>();
-        for (int v = 0; v < gridhView.getChildCount(); v++) {
-            bindings.add(((BindingView) gridhView.getChildAt(v)).getBinding());
-        }
-        return bindings;
-    }
+	@NonNull
+	private List<ValueBinding> readFromGrid() {
+		List<ValueBinding> bindings = new ArrayList<>();
+		for (int v = 0; v < gridhView.getChildCount(); v++) {
+			bindings.add(((BindingView) gridhView.getChildAt(v)).getBinding());
+		}
+		return bindings;
+	}
 
-    @Override
-    protected void onDestroy() {
-        List<ValueBinding> bindings = readFromGrid();
-        bindingPreference.setList(bindings);
+	@Override
+	protected void onDestroy() {
+		List<ValueBinding> bindings = readFromGrid();
+		bindingPreference.setList(bindings);
 
-        if (paceBoat != null) {
-            paceBoat.dismiss();
-            paceBoat = null;
-        }
+		paceBoat = null;
 
-        super.onDestroy();
-    }
+		super.onDestroy();
+	}
 
-    @Override
-    public void onResume() {
-        super.onResume();
+	@Override
+	public void onResume() {
+		super.onResume();
 
-        changed();
-        gym.addListener(this);
-    }
+		changed();
+		gym.addListener(this);
+	}
 
-    @Override
-    protected void onPause() {
-        gym.removeListener(this);
+	@Override
+	protected void onPause() {
+		gym.removeListener(this);
 
-        super.onPause();
-    }
+		super.onPause();
+	}
 
-    @Override
-    public void changed() {
-        if (gym.program == null) {
-            finish();
-            return;
-        }
+	@Override
+	public void changed() {
+		if (gym.program == null) {
+			finish();
+			return;
+		}
 
-        updateBindings();
-        updateLevel();
-    }
+		updateBindings();
+		updateLevel();
+	}
 
-    @Override
-    public void onSystemUiVisibilityChange(int visibility) {
-        leanBack(true);
-    }
+	@Override
+	public void onSystemUiVisibilityChange(int visibility) {
+		leanBack(true);
+	}
 
-    private void updateBindings() {
-        int count = gridhView.getChildCount();
-        for (int v = 0; v < count; v++) {
-            ((BindingView) gridhView.getChildAt(v)).changed(gym, paceBoat);
-        }
-    }
+	private void updateBindings() {
+		int count = gridhView.getChildCount();
+		for (int v = 0; v < count; v++) {
+			((BindingView) gridhView.getChildAt(v)).changed(gym, paceBoat);
+		}
+	}
 
-    private void updateLevel() {
-        float value = 0f;
-        float total = 0f;
-        Gym.Progress progress = gym.progress;
-        for (Segment segment : gym.program.segments.get()) {
-            float segmentValue = segment.asDuration();
+	private void updateLevel() {
+		float value = 0f;
+		float total = 0f;
+		Gym.Progress progress = gym.progress;
+		for (Segment segment : gym.program.segments.get()) {
+			float segmentValue = segment.asDuration();
 
-            if (progress != null && progress.segment == segment) {
-                value = total + progress.completion() * segmentValue;
-            }
+			if (progress != null && progress.segment == segment) {
+				value = total + progress.completion() * segmentValue;
+			}
 
-            total += segmentValue;
-        }
-        if (progress == null) {
-            value = total;
-        }
-        progressView.setLevel(Math.round(value * 10000 / total));
-    }
+			total += segmentValue;
+		}
+		if (progress == null) {
+			value = total;
+		}
+		progressView.setLevel(Math.round(value * 10000 / total));
+	}
 
-    @Override
-    public void onBinding(int index, ValueBinding binding) {
-        if (binding != null) {
-            ((BindingView) gridhView.getChildAt(index)).setBinding(binding);
-        }
+	@Override
+	public void onBinding(int index, ValueBinding binding) {
+		if (binding != null) {
+			((BindingView) gridhView.getChildAt(index)).setBinding(binding);
+		}
 
-        leanBack(true);
-    }
+		leanBack(true);
+	}
 
-    private void leanBack(boolean yes) {
-        if (yes) {
-            getWindow().getDecorView().postDelayed(returnToLeanBack, 3000);
-        } else {
-            getWindow().getDecorView().getHandler().removeCallbacks(returnToLeanBack);
-        }
-    }
+	private void leanBack(boolean yes) {
+		if (yes) {
+			getWindow().getDecorView().postDelayed(returnToLeanBack, 3000);
+		} else {
+			getWindow().getDecorView().getHandler().removeCallbacks(returnToLeanBack);
+		}
+	}
 
-    public static void start(Activity activity) {
+	public static void start(Activity activity) {
 
-        Preference<Boolean> intentPreference = Preference.getBoolean(activity, R.string.preference_integration_intent);
-        if (intentPreference.get()) {
-            Preference<String> intentUriPreference = Preference.getString(activity, R.string.preference_integration_intent_uri);
-            String uri = intentUriPreference.get();
+		Preference<Boolean> intentPreference = Preference.getBoolean(activity, R.string.preference_integration_intent);
+		if (intentPreference.get()) {
+			Preference<String> intentUriPreference = Preference.getString(activity, R.string.preference_integration_intent_uri);
+			String uri = intentUriPreference.get();
 
-            try {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-                activity.startActivity(intent);
-                return;
-            } catch (Exception ex) {
-            }
-        }
+			try {
+				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+				activity.startActivity(intent);
+				return;
+			} catch (Exception ex) {
+			}
+		}
 
-        restart(activity);
-    }
+		restart(activity);
+	}
 
-    public static void restart(Activity activity) {
-        activity.startActivity(new Intent(activity, WorkoutActivity.class));
-    }
-
-    private interface DismissablePaceBoat extends BindingView.PaceBoat {
-        void dismiss();
-    }
+	public static void restart(Activity activity) {
+		activity.startActivity(new Intent(activity, WorkoutActivity.class));
+	}
 
 	/**
-     * Use a previous {@link svenmeier.coxswain.gym.Workout} as a pace boat.
-     */
-    private class WorkoutPaceBoat extends MatchLookup<Snapshot> implements DismissablePaceBoat {
+	 * Use a previous {@link svenmeier.coxswain.gym.Workout} as a pace boat.
+	 */
+	private class WorkoutPaceBoat extends MatchLookup<Snapshot> implements BindingView.PaceBoat {
 
-        private List<Snapshot> snapshots = new ArrayList<>();
+		private List<Snapshot> snapshots = new ArrayList<>();
 
-        private int duration;
+		private int duration;
 
-        protected WorkoutPaceBoat() {
-            super(gym.getSnapshots(gym.pace));
+		protected WorkoutPaceBoat() {
+			super(gym.getSnapshots(gym.pace));
 
-            restartLoader(0, WorkoutActivity.this);
-        }
+			restartLoader(0, WorkoutActivity.this);
+		}
 
-        @Override
-        public int getDistanceDelta(Measurement measurement) {
-            if (snapshots.isEmpty()) {
-                return 0;
-            }
+		@Override
+		public int getDistanceDelta(Measurement measurement) {
+			if (snapshots.isEmpty()) {
+				return 0;
+			}
 
-            int from = distanceAt(measurement.duration);
-            int to = distanceAt(measurement.duration + 1);
+			int from = distanceAt(measurement.duration);
+			int to = distanceAt(measurement.duration + 1);
 
-            int paceDistance = from + ((to - from) * (measurement.distance % 1000) / 1000);
+			int paceDistance = from + ((to - from) * (measurement.distance % 1000) / 1000);
 
-            return measurement.distance - paceDistance;
-        }
+			return measurement.distance - paceDistance;
+		}
 
-        private int distanceAt(int duration) {
-            // increment by one, because first snapshot is written after one second only
-            int index = duration - 1;
-            if (index < 0) {
-                return 0;
-            }
+		private int distanceAt(int duration) {
+			// increment by one, because first snapshot is written after one second only
+			int index = duration - 1;
+			if (index < 0) {
+				return 0;
+			}
 
-            if (index >= snapshots.size()) {
-                index = snapshots.size() - 1;
-            }
-            return snapshots.get(index).distance.get();
-        }
+			if (index >= snapshots.size()) {
+				index = snapshots.size() - 1;
+			}
+			return snapshots.get(index).distance.get();
+		}
 
-        @Override
-        public int getDurationDelta(Measurement measurement) {
-            while (this.duration < snapshots.size()) {
-                if (snapshots.get(this.duration).distance.get() >= measurement.distance) {
-                    break;
-                }
+		@Override
+		public int getDurationDelta(Measurement measurement) {
+			while (this.duration < snapshots.size()) {
+				if (snapshots.get(this.duration).distance.get() >= measurement.distance) {
+					break;
+				}
 
-                this.duration++;
-            }
+				this.duration++;
+			}
 
-            if (this.duration >= snapshots.size() && this.duration > 0) {
-                Snapshot snapshot = snapshots.get(snapshots.size() - 1);
+			if (this.duration >= snapshots.size() && this.duration > 0) {
+				Snapshot snapshot = snapshots.get(snapshots.size() - 1);
 
-                int distance = snapshot.distance.get();
-                if (distance > 0) {
-                    // estimate duration
-                    this.duration = snapshots.size() * measurement.distance / distance;
-                }
-            }
+				int distance = snapshot.distance.get();
+				if (distance > 0) {
+					// estimate duration
+					this.duration = snapshots.size() * measurement.distance / distance;
+				}
+			}
 
-            return measurement.duration - this.duration;
-        }
+			return measurement.duration - this.duration;
+		}
 
-        @Override
-        protected void onLookup(List<Snapshot> propoids) {
-            this.snapshots = propoids;
+		@Override
+		protected void onLookup(List<Snapshot> propoids) {
+			this.snapshots = propoids;
 
-            // no updates needed
-            dismiss();
-        }
-
-        @Override
-        public void dismiss() {
-            destroy(0, WorkoutActivity.this);
-        }
-    }
+			// no updates needed
+			destroy(0, WorkoutActivity.this);
+		}
+	}
 
 	/**
-     * Use self as pace boat.
-     */
-    private class SelfPaceBoat implements DismissablePaceBoat {
+	 * Use self as pace boat.
+	 */
+	private class SelfPaceBoat implements BindingView.PaceBoat {
 
-        @Override
-        public int getDistanceDelta(Measurement measurement) {
-            return 0;
-        }
+		@Override
+		public int getDistanceDelta(Measurement measurement) {
+			return 0;
+		}
 
-        @Override
-        public int getDurationDelta(Measurement measurement) {
-            return 0;
-        }
-
-        @Override
-        public void dismiss() {
-        }
-    }
+		@Override
+		public int getDurationDelta(Measurement measurement) {
+			return 0;
+		}
+	}
 }
